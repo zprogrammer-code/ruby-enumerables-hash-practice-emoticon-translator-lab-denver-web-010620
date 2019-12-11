0d4e8e73fa0c3e322322da4743dbbbc9fc89a772
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-11 05:08:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,7 @@ end
 def get_japanese_emoticon(file, emoticon)
   japanese_emoticons = load_library(file)
 sorry_message = "Sorry, that emoticon was not found"
+
 if arguement is not a known emoticon
   return sorry_message
 else
